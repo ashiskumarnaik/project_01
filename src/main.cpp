9940f76484410069d5d11278a7fefc312323ec00
Ashis Kumar Naik <ashishami2002@gmail.com>
2025-05-26 00:36:00 +0530

    first cmake execute
    Signed-off-by: Ashis Kumar Naik <ashishami2002@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "mylib.hpp"
+
+int main() {
+    std::cout << greet("CMake World") << "\n";
+    return 0;
+}
---
+++ src/mylib.cpp
@@ -0,0 +1,5 @@
+#include "mylib.hpp"
+
+std::string greet(std::string const& who) {
+    return "Hello, " + who + "!";
+}
